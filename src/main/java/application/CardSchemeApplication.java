acdf5914863eeb88cd65e7142aa5df78256ed1d8
vikash <vkpgupta@gmail.com>
2021-02-18 01:30:54 +0530

    Initializing project

---
+++ src/main/java/application/CardSchemeApplication.java
@@ -0,0 +1,13 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardSchemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardSchemeApplication.class, args);
+	}
+
+}
