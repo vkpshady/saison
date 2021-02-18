2fc2d745af2917fb09ec737aa05c99eacfb9109c
vikash <vkpgupta@gmail.com>
2021-02-18 15:58:40 +0530

    Modified service and application class

--- src/main/java/application/CardSchemeApplication.java
+++ src/main/java/application/CardSchemeApplication.java
@@ -1,9 +1,17 @@
 package application;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EntityScan("com")
+@ComponentScan("com")
+@EnableJpaRepositories("com.repository")
 public class CardSchemeApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/service/CardServiceImpl.java
+++ src/main/java/com/service/CardServiceImpl.java
@@ -16,61 +16,62 @@ import com.entity.CardHitDetail;
 
 @Service
 public class CardServiceImpl implements CardService {
-	private final String thridPartyURL="https://lookup.binlist.net/";
-	
-	@Autowired
-	CardRepository cardRepository;
-	
-	
-	@Override
-	public ResponseEntity<CardInformation> getDetails(final String cardId) {
-		
-		RestTemplate restTemplate = new RestTemplate();
-		String resourceUrl = thridPartyURL.concat(cardId);
-		
-		CardInformation cardInfo;
-		ResponseEntity<CardInformation> response;
-		
-		try {
-			cardInfo= restTemplate.getForObject(resourceUrl , CardInformation.class);
-			response=new ResponseEntity<>(cardInfo,HttpStatus.OK);
-		}catch(Exception e){
-			response=new ResponseEntity<>(HttpStatus.NOT_FOUND);
-			return response;
-		}
-		
-		updateHitCount(cardId);
-//		
-//		System.out.println(cardInfo.toString());
-//		System.out.println(cardRepository.findById(cardId).get().toString());
-		return response;
-		
-	}
-	
-	
-	private void updateHitCount(String cardId) { 
-		Optional<CardHitDetail>  currentCard=cardRepository.findById(cardId); 
-		CardHitDetail  currentCardHitDetail; 
-		if(currentCard.isPresent()) {
-			currentCardHitDetail=currentCard.get(); 
-			currentCardHitDetail.setHitCount(currentCardHitDetail.getHitCount()+1); 
-		}
-		else { 
-			currentCardHitDetail=new CardHitDetail(); 
-			currentCardHitDetail.setCardId(cardId); 
-			currentCardHitDetail.setHitCount(1);
-		}
-		
-		cardRepository.save(currentCardHitDetail);
-	 }
-	
-	
-	@Override
-	public List<CardHitDetail> getStats(int start,int limit) {
-		List<CardHitDetail> cardList=cardRepository.findStats(limit, start-1);
-		return cardList;
-	}
-	
-	
-	
+    private final String thridPartyURL = "https://lookup.binlist.net/";
+
+    @Autowired
+    CardRepository cardRepository;
+
+    /*
+    To fetch card details from third party API
+    */
+    @Override
+    public ResponseEntity<CardInformation> getDetails(final String cardId) {
+
+        RestTemplate restTemplate = new RestTemplate();
+        String resourceUrl = thridPartyURL.concat(cardId);
+
+        CardInformation cardInfo;
+        ResponseEntity<CardInformation> response;
+
+        try {
+            cardInfo = restTemplate.getForObject(resourceUrl, CardInformation.class);
+            response = new ResponseEntity<>(cardInfo, HttpStatus.OK);
+        } catch (Exception e) {
+            response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return response;
+        }
+
+        updateHitCount(cardId);
+        return response;
+
+    }
+
+    /*
+    To update card hit count in table
+    * */
+    private void updateHitCount(String cardId) {
+        Optional<CardHitDetail> currentCard = cardRepository.findById(cardId);
+        CardHitDetail currentCardHitDetail;
+        if (currentCard.isPresent()) {
+            currentCardHitDetail = currentCard.get();
+            currentCardHitDetail.setHitCount(currentCardHitDetail.getHitCount() + 1);
+        } else {
+            currentCardHitDetail = new CardHitDetail();
+            currentCardHitDetail.setCardId(cardId);
+            currentCardHitDetail.setHitCount(1);
+        }
+
+        cardRepository.save(currentCardHitDetail);
+    }
+
+    /*
+    To fetch data from CardHitDetail table
+    */
+    @Override
+    public List<CardHitDetail> getStats(int start, int limit) {
+        List<CardHitDetail> cardList = cardRepository.findStats(limit, start - 1);
+        return cardList;
+    }
+
+
 }
